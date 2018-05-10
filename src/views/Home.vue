77e6a6e4435a486b713fd6b7a76d594048d8edc5
朋友了 <462716549@qq.com>
2018-05-10 16:07:15 +0800

    首次上传

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="height:100%;">
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script type="es6">
+
+  export default {
+    components: {},
+    data() {
+      return {}
+    },
+    methods: {
+      //设置底栏高亮项
+      active() {
+        return '/' + this.$route.path.split("/")[1];
+      },
+    },
+    mounted() {
+
+    }
+  }
+</script>
+<style>
+
+</style>
