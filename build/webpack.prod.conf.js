35efa9bb5f2a396447ef5454e0665ffa8d31566b
凡人 <462716549@qq.com>
2018-05-14 16:28:18 +0800

    开发和发布环境config.js的引入区分

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -53,8 +53,8 @@ const webpackConfig = merge(baseWebpackConfig, {
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
       cssProcessorOptions: config.build.productionSourceMap
-      ? { safe: true, map: { inline: false } }
-      : { safe: true }
+        ? {safe: true, map: {inline: false}}
+        : {safe: true}
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
@@ -63,7 +63,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
         : config.build.index,
-      template: 'index.html',
+      template: 'index-pro.html',
       inject: true,
       minify: {
         removeComments: true,
